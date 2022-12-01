490a95261c15936e7e56785eacba5d8888c188a3
Milan <igrok5554545@gmail.com>
2022-12-01 16:26:50 +0300

    Add variables to class

--- src/Schemes/ABList.php
+++ src/Schemes/ABList.php
@@ -6,13 +6,28 @@ use ABAPI\Tools\SchemesConverter;
 
 class ABList extends SchemesConverter
 {
+    /**
+     * @var string $name
+     */
     public $name = '';
 
+    /**
+     * @var int $name
+     */
     public $messages_count_per_account = 0;
 
+    /**
+     * @var int $name
+     */
     public $messages_delay = 0;
 
+    /**
+     * @var string $name
+     */
     public $text = '';
 
+    /**
+     * @var string $name
+     */
     public $deadline = '';
 }
