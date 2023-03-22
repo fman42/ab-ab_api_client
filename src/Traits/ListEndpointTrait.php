7d0243d13b1ecefae948e51872b1916705e35a76
Milan <igrok5554545@gmail.com>
2023-03-22 11:11:43 +0300

    Fix

--- src/Traits/ListEndpointTrait.php
+++ src/Traits/ListEndpointTrait.php
@@ -6,6 +6,12 @@ use ABAPI\Schemes\HttpRequest;
 
 trait ListEndpointTrait
 {
+    /**
+     * @param int $list_id
+     * @param string $fieldName
+     * @param string $fieldValue
+     * @return HttpRequest
+     */
     private function makeUpdateListTargetFieldRequest(int $list_id, string $fieldName, string $fieldValue)
     {
         return new HttpRequest('list', [
